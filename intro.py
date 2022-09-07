3ffbc0382ceaa2fdd71ff26e2b89103b1d12f217
anjan shrestha <anjanxtha46@gmail.com>
2022-09-07 07:58:44 +0545

    project started

---
+++ intro.py
@@ -0,0 +1,56 @@
+"main thing"
+
+#the is a single line commen(single line comment)
+"""
+this is a multi line comment
+we can write many lines here
+as much as you want 
+no matter
+This all above lines are consider as comment in python
+"""
+#this is a print funciton in python
+#which provides output to the console 
+from posixpath import split
+from tkinter.font import names
+from unicodedata import name
+
+
+print("hello! batch 33 B") 
+
+
+# Button calling --> Dail
+def phone_num():
+    pass
+
+
+print(f"{phone_num} \n calling...")
+
+def num_length():
+    pass
+def openConatctDialer(num_length):
+ if num_length >10:
+        print("Number Invalid")
+ elif num_length <10:
+        print("end call")
+ elif num_length==10:
+            dail(num_length)
+ else:
+    return
+
+    recipent_num = input("please provide number ")
+openConatctDialer(int(len("recipent_num")))
+
+
+#contiuing
+name = input("Please provide your name")
+print(f"Hi!{name},Hpw are you?") #string formating
+
+
+name,Age=input("please provoide your name and Age (comma string)") ("split")
+print(f"H!your name is {name},and your are {Age} years old") #string formation
+
+
+#
+
+
+
