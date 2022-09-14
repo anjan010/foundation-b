35875a8ceea5e51ca7aefb52e5d7ae3acb143981
anjan shrestha <anjanxtha46@gmail.com>
2022-09-14 08:12:11 +0545

    fps.py

---
+++ fps.py
@@ -0,0 +1,15 @@
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+print(f"add sum of {a+b}")
+
+
+name=(input("enter the company name"))
+model=(input("enter the model name"))
+price=(input("enter the excat price of the laptop"))
+
+print(f"EX: {name} {model} @ RS {price}")
+
+
+
